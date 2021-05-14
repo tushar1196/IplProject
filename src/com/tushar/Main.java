1fca42da31b6110878751c941c5559a1aecbf4a5
tusharchavhan111196 <tusharchavhan111196@gmail.com>
2021-05-14 13:07:26 +0530

    Getting data

--- src/com/tushar/Main.java
+++ src/com/tushar/Main.java
@@ -1,8 +1,83 @@
 package com.tushar;
 
+import com.tushar.Model.Delivery;
+import com.tushar.Model.Match;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    public static final int DELIVERY_BLOWING_TEAM = 3;
+    public static final int DELIVERY_TOTAL_RUN = 17;
+    public static final int DELIVERY_EXTRA_RUN = 16;
+    public static final int DELIVERY_BLOWER = 8;
+    public static final int DELIVERY_BALL = 5;
+    public static final int DELIVERY_OVER = 4;
+    public static final int DELIVERY_MATCH_ID = 0;
+    public static final int MATCH_WINNER = 10;
+    public static final int MATCH_TOSS_WINNER = 6;
+    public static final int SEASON = 1;
+    public static final int MATCH_ID = 0;
+    private static final int MATCH_CITY = 2;
 
     public static void main(String[] args) {
-	// write your code here
+        List<Match> matches = getMatchData();
+        List<Delivery> deliveries = getDeliveryData();
+
+        
+    }
+
+    private static List<Delivery> getDeliveryData() {
+        System.out.println("Delivery Data read");
+        List<Delivery> deliveries = new LinkedList<Delivery>();
+        String line = "";
+        try {
+            final BufferedReader br = new BufferedReader(new FileReader("E:\\mountblue\\Ipl project\\archive\\deliveries.csv"));
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+
+                Delivery delivery = new Delivery();
+                delivery.setDeliveryMatchId(fields[DELIVERY_MATCH_ID]);
+                delivery.setDeliveryOver(fields[DELIVERY_OVER]);
+                delivery.setDeliveryBall(fields[DELIVERY_BALL]);
+                delivery.setDeliveryBlower(fields[DELIVERY_BLOWER]);
+                delivery.setDeliveryExtraRuns(fields[DELIVERY_EXTRA_RUN]);
+                delivery.setDeliveryTotalRuns(fields[DELIVERY_TOTAL_RUN]);
+                delivery.setDeliveryBlowingTeam(fields[DELIVERY_BLOWING_TEAM]);
+
+                deliveries.add(delivery);
+            }
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+        return deliveries;
+
+    }
+
+    private static List<Match> getMatchData() {
+        System.out.println("Match Data read");
+        final List<Match> matches = new LinkedList<Match>();
+        String line = "";
+        try {
+            final BufferedReader br = new BufferedReader(new FileReader("E:\\mountblue\\Ipl project\\archive\\matches.csv"));
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+
+                Match match = new Match();
+                match.setMatchId(fields[MATCH_ID]);
+                match.setMatchSeason(fields[SEASON]);
+                match.setMatchTossWinner(fields[MATCH_TOSS_WINNER]);
+                match.setMatchwinner(fields[MATCH_WINNER]);
+                match.setMatchCity(fields[MATCH_CITY]);
+
+                matches.add(match);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return matches;
     }
 }
--- src/com/tushar/Model/Delivery.java
+++ src/com/tushar/Model/Delivery.java
@@ -1,4 +1,67 @@
 package com.tushar.Model;
 
 public class Delivery {
+    private String deliveryMatchId;
+    private String deliveryOver;
+    private String deliveryBall;
+    private String deliveryBlower;
+    private String deliveryExtraRuns;
+    private String deliveryTotalRuns;
+    private String deliveryBlowingTeam;
+
+    public String getDeliveryMatchId() {
+        return deliveryMatchId;
+    }
+
+    public void setDeliveryMatchId(String deliveryMatchId) {
+        this.deliveryMatchId = deliveryMatchId;
+    }
+
+    public String getDeliveryOver() {
+        return deliveryOver;
+    }
+
+    public void setDeliveryOver(String deliveryOver) {
+        this.deliveryOver = deliveryOver;
+    }
+
+    public String getDeliveryBall() {
+        return deliveryBall;
+    }
+
+    public void setDeliveryBall(String deliveryBall) {
+        this.deliveryBall = deliveryBall;
+    }
+
+    public String getDeliveryBlower() {
+        return deliveryBlower;
+    }
+
+    public void setDeliveryBlower(String deliveryBlower) {
+        this.deliveryBlower = deliveryBlower;
+    }
+
+    public String getDeliveryExtraRuns() {
+        return deliveryExtraRuns;
+    }
+
+    public void setDeliveryExtraRuns(String deliveryExtraRuns) {
+        this.deliveryExtraRuns = deliveryExtraRuns;
+    }
+
+    public String getDeliveryTotalRuns() {
+        return deliveryTotalRuns;
+    }
+
+    public void setDeliveryTotalRuns(String deliveryTotalRuns) {
+        this.deliveryTotalRuns = deliveryTotalRuns;
+    }
+
+    public String getDeliveryBlowingTeam() {
+        return deliveryBlowingTeam;
+    }
+
+    public void setDeliveryBlowingTeam(String deliveryBlowingTeam) {
+        this.deliveryBlowingTeam = deliveryBlowingTeam;
+    }
 }
