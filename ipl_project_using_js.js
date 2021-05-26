f2dfbd8902647c6b525ba4794ff14d87b88a712d
Tushar Chavhan <tusharchavhan111196@gmail.com>
2021-05-26 13:53:53 +0530

    getDeliveriesDataComplete

--- ipl_project_using_js.js
+++ ipl_project_using_js.js
@@ -6,7 +6,13 @@ const MATCH_TOSS_WINNER = 6;
 const SEASON = 1;
 const MATCH_ID = 0;
 const MATCH_CITY = 2;
-
+const DELIVERY_BLOWING_TEAM = 3;
+const DELIVERY_TOTAL_RUN = 17;
+const DELIVERY_EXTRA_RUN = 16;
+const DELIVERY_BLOWER = 8;
+const DELIVERY_BALL = 5;
+const DELIVERY_OVER = 4;
+const DELIVERY_MATCH_ID = 0;
 
 
 
@@ -14,6 +20,7 @@ const MATCH_CITY = 2;
 
 getMatchesData();
 getDeliveriesData();
+findExtraRunsConcededPerTeamIn2016(matches, deliveries);
 
 
 
@@ -48,15 +55,22 @@ getDeliveriesData();
 
 
 
-
-
-
-
-
-
-
-
-
+function getDeliveriesData() {
+    const fs = require('fs');
+    var deliveriesDataInBuffer = fs.readFileSync('deliveries.csv');
+    var deliveriesDataArray = deliveriesDataInBuffer.toString().split(/\r?\n/);
+    deliveriesDataArray.forEach(element => {
+        var delivery={};
+        delivery.matchId=element.split(",")[DELIVERY_MATCH_ID];
+        delivery.blowingTeam=element.split(",")[DELIVERY_BLOWING_TEAM];
+        delivery.totalRuns=element.split(",")[DELIVERY_TOTAL_RUN];
+        delivery.extraRuns=element.split(",")[DELIVERY_EXTRA_RUN];
+        delivery.blower=element.split(",")[DELIVERY_BLOWER];
+        delivery.ball=element.split(",")[DELIVERY_BALL];
+        delivery.over=element.split(",")[DELIVERY_OVER];
+        deliveries.push(delivery);
+    });
+}
 
 function getMatchesData() {
     const fs = require('fs');
